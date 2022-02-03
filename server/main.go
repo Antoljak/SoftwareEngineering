9d41ae099e9b5ac856d8a73e6107ced0d4d1d522
Praveena-H <63477936+Praveena-H@users.noreply.github.com>
2022-02-02 19:47:11 -0500

    updated read, create and delete methods

--- server/main.go
+++ server/main.go
@@ -9,19 +9,52 @@ import (
 	"google.golang.org/api/option"
 )
 
-
 type FileData struct {
 	content string `firestore:"content"`
 	label   string `firestore:"label"`
 }
 
-func constructFileData(){
+var client *firestore.Client
+var err error
+var ctx context.Context
+var fileContent = FileData{
+	content: "Loaded from main",
+	label:   "green",
+}
+var doc = make(map[string]interface{})
+var docRef string
+var docRefDel string
+var docRefRead string
+
+func main() {
+	fmt.Println("hello world")
+	readFire(docRefRead)
+	setFire(docRef, doc)
+	deleteFire(docRefDel)
+	defer client.Close()
+}
+
+// Simple init to have a firestore client available
+func init() {
+	ctx = context.Background()
+	opt := option.WithCredentialsFile("note-it-db-service-account")
+	client, err = firestore.NewClient(ctx, "note-it-db", opt)
+	if err != nil {
+		log.Fatalf("Firestore: %v", err)
+	}
+	doc["objectExample"] = map[string]interface{}{
+		"content": "sample",
+		"label":   "green",
+	}
+	docRefRead = "Users/user1/user1Files/file1"
+	docRef = "Users/user1/user1Files/file7"
+	docRefDel = "Users/user1/user1Files/file2"
 
 }
 
-func readFire(docRef string){ 
+//Function to read a file from database
+func readFire(docRef string) {
 	fmt.Println("inside readFire()")
-	docRef = "Users/user1/user1Files/file1" //temporary
 	users, err := client.Doc(docRef).Get(ctx)
 	if err != nil {
 		log.Fatalf("Firestore: %v", err)
@@ -31,48 +64,20 @@ func readFire(docRef string){
 	}
 }
 
-func setFire(docRef string, fileContent FileData){
-	//Create and Update - Note: If file already exists, Create will throw error
-	// file5Data := FileData{
-	// 	Content: "Loaded from main",
-	// 	Label:   "Blue",
-	// }
-	docRef = "Users/user1/user1Files/file5"
-	wr, err := client.Doc(docRef).Set(ctx, fileContent)
+//Function to create a new file or update if the file already exists
+func setFire(docRef string, doc map[string]interface{}) {
+	fmt.Println("inside setFire()")
+	_, err := client.Doc(docRef).Set(ctx, doc)
 	if err != nil {
 		log.Fatalf("Firestore: %v", err)
 	}
 }
 
-func deleteFire(docRef string){
-	//Delete document
-	docRef = "Users/user1/user1Files/file6"
+//Function to delete a file from database
+func deleteFire(docRef string) {
+	fmt.Println("inside deleteFire")
 	_, err = client.Doc(docRef).Delete(ctx)
 	if err != nil {
 		log.Fatalf("Firestore: %v", err)
 	}
-}
-
-// Simple init to have a firestore client available
-func init() {
-	fmt.Println("testing the new thing \n")
-	ctx := context.Background()
-	var err error
-	opt := option.WithCredentialsFile("note-it-db-service-account.json")
-	client, err := firestore.NewClient(ctx, "note-it-db", opt)
-	if err != nil {
-		log.Fatalf("Firestore: %v", err)
-	}
-	defer client.Close()
-}
-
-func main() {
-	fmt.Println("hello world")
-
-	readFire("test")
-	
-	
-	// fmt.Println(wr.UpdateTime)
-
-	
 }
