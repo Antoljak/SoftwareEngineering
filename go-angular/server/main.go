fa123ee5dbcb2f2a1e3ede4aa80b2ae36833b11c
camusic17 <33007703+camusic17@users.noreply.github.com>
2022-02-09 13:42:09 -0500

    refactored sprint 1 code to fix mode modules

--- go-angular/server/main.go
+++ go-angular/server/main.go
@@ -1,22 +1,83 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
-	"github.com/jinzhu/gorm"
-	"os"
+	"context"
+	"fmt"
+	"log"
+
+	"cloud.google.com/go/firestore"
+	"google.golang.org/api/option"
 )
 
+type FileData struct {
+	content string `firestore:"content"`
+	label   string `firestore:"label"`
+}
+
+var client *firestore.Client
+var err error
+var ctx context.Context
+var fileContent = FileData{
+	content: "Loaded from main",
+	label:   "green",
+}
+var doc = make(map[string]interface{})
+var docRef string
+var docRefDel string
+var docRefRead string
+
 func main() {
-	pass := os.Getenv("DB_PASS")
-	db, err := gorm.Open(
-		"postgres",
-		"host=students-db user=go password="+pass+" dbname=go sslmode=disable")
+	fmt.Println("hello world")
+	readFire(docRefRead)
+	setFire(docRef, doc)
+	deleteFire(docRefDel)
+	defer client.Close()
+}
+
+// Simple init to have a firestore client available
+func init() {
+	ctx = context.Background()
+	opt := option.WithCredentialsFile("note-it-db-service-account.json")
+	client, err = firestore.NewClient(ctx, "note-it-db", opt)
 	if err != nil {
-		panic(err.Error())
+		log.Fatalf("Firestore: %v", err)
 	}
-	app := App{
-		db: db,
-		r:  mux.NewRouter(),
+	doc["objectExample"] = map[string]interface{}{
+		"content": "sample",
+		"label":   "green",
 	}
-	app.start()
+	docRefRead = "Users/user1/user1Files/file1"
+	docRef = "Users/user1/user1Files/file7"
+	docRefDel = "Users/user1/user1Files/file2"
+
+}
+
+//Function to read a file from database
+func readFire(docRef string) {
+	fmt.Println("inside readFire()")
+	users, err := client.Doc(docRef).Get(ctx)
+	if err != nil {
+		log.Fatalf("Firestore: %v", err)
+	}
+	for key, element := range users.Data() {
+		fmt.Println("Key:", key, "=>", "Element:", element)
+	}
+}
+
+//Function to create a new file or update if the file already exists
+func setFire(docRef string, doc map[string]interface{}) {
+	fmt.Println("inside setFire()")
+	_, err := client.Doc(docRef).Set(ctx, doc)
+	if err != nil {
+		log.Fatalf("Firestore: %v", err)
+	}
+}
+
+//Function to delete a file from database
+func deleteFire(docRef string) {
+	fmt.Println("inside deleteFire")
+	_, err = client.Doc(docRef).Delete(ctx)
+	if err != nil {
+		log.Fatalf("Firestore: %v", err)
+	}
 }
