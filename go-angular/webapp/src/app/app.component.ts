bb53b24b6601177ecd19e323b6874703e33e1057
camusic17 <33007703+camusic17@users.noreply.github.com>
2022-02-23 14:17:47 -0500

    added 2-wau data dinding with sourceData var

--- go-angular/webapp/src/app/app.component.ts
+++ go-angular/webapp/src/app/app.component.ts
@@ -1,50 +1,83 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit, ViewChild, AfterViewInit,	ElementRef} from '@angular/core';
 import {MatDialog} from "@angular/material/dialog";
 import {StudentDialog} from "./student-dialog/student-dialog.component";
 import {StudentsService} from "./students.service";
 import {Student} from "./student";
 import {Subscription} from "rxjs";
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, OnDestroy {
-  displayedColumns = ['name', 'age'];
-  dataSource: Student[] = [];
-  getAllSubscription: Subscription;
-  dialogSubscription: Subscription;
+export class AppComponent implements AfterViewInit {
+  @ViewChild( 'editor',{static:false} ) editor;
 
-  constructor(public dialog: MatDialog, public service: StudentsService) {}
+  public editorData = '<p>Note it!</p>'
+	public isSourceActive: boolean;
+	public sourceData: string;
+  // displayedColumns = ['name', 'age'];
+  // dataSource: Student[] = [];
+  // getAllSubscription: Subscription;
+  // dialogSubscription: Subscription;
 
-  openEditDialog(st: Student) {
-    this.openDialog(new Student(st.id, st.name, st.age));
-  }
+  constructor(public dialog: MatDialog) {}
 
-  openNewDialog(): void {
-    this.openDialog(new Student());
-  }
+  // openEditDialog(st: Student) {
+  //   this.openDialog(new Student(st.id, st.name, st.age));
+  // }
 
-  private openDialog(st: Student): void {
-    this.dialogSubscription = this.dialog
-      .open(StudentDialog, {data: st, minWidth: '30%'})
-      .afterClosed().subscribe(() => this.loadStudentsList());
-  }
+  // openNewDialog(): void {
+  //   this.openDialog(new Student());
+  // }
 
-  private loadStudentsList(): void {
-    this.getAllSubscription = this.service.getAll()
-      .subscribe(students => this.dataSource = students);
-  }
+  // private openDialog(st: Student): void {
+  //   this.dialogSubscription = this.dialog
+  //     .open(StudentDialog, {data: st, minWidth: '30%'})
+  //     .afterClosed().subscribe(() => this.loadStudentsList());
+  // }
 
-  ngOnInit(): void {
-    this.loadStudentsList();
-  }
+  // private loadStudentsList(): void {
+  //   this.getAllSubscription = this.service.getAll()
+  //     .subscribe(students => this.dataSource = students);
+  // }
 
-  ngOnDestroy(): void {
-    this.getAllSubscription.unsubscribe();
-    if (this.dialogSubscription) {
-      this.dialogSubscription.unsubscribe();
-    }
-  }
+  // ngOnInit(): void {
+  //   this.loadStudentsList();
+  // }
+
+  // ngOnDestroy(): void {
+  //   this.getAllSubscription.unsubscribe();
+  //   if (this.dialogSubscription) {
+  //     this.dialogSubscription.unsubscribe();
+  //   }
+  // }
+
+  ngAfterViewInit() {
+		this.editor.dataChange.subscribe( ( value ) => {
+			if ( !this.isSourceActive ) {
+				this.sourceData = value;
+			}
+		} );
+	}
 }
+
+// export class EditorBindingComponent implements AfterViewInit {
+// 	@ViewChild( 'editor',{static:false} ) editor;
+
+// 	public editorData = '<p>This is a CKEditor 4 WYSIWYG editor instance created with Angular.</p>'
+
+// 	public isSourceActive: boolean;
+
+// 	public sourceData: string;
+
+// 	ngAfterViewInit() {
+// 		this.editor.dataChange.subscribe( ( value ) => {
+// 			if ( !this.isSourceActive ) {
+// 				this.sourceData = value;
+// 			}
+// 		} );
+// 	}
+// }
