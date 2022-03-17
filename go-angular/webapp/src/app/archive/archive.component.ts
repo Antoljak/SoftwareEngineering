b017043fe1a5d2ec6d288479ada5af52921158f0
camusic17 <33007703+camusic17@users.noreply.github.com>
2022-03-16 20:26:53 -0400

    added toolbar and basic navigation to archive page

--- go-angular/webapp/src/app/archive/archive.component.ts
+++ go-angular/webapp/src/app/archive/archive.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+import { AngularFireAuth, } from '@angular/fire/auth';
+
 
 @Component({
   selector: 'app-archive',
@@ -6,12 +10,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./archive.component.scss']
 })
 export class ArchiveComponent implements OnInit {
+  public userEmail = "";
 
-  constructor() { }
+  constructor(private router: Router, public afAuth: AngularFireAuth) {}
 
-  ngOnInit() {
+  ngOnInit() { }
 
-    
-  }
+  getUserEmail() {
+		this.userEmail = this.afAuth.auth.currentUser.email;
+		if(this.userEmail != null){
+			return this.userEmail;
+		}
+		else{
+			this.userEmail = "Guest"
+			return this.userEmail;
+		}
+	}
+
+  NewNote() {
+		console.log("Loading editor for new note...");
+		this.router.navigate([`editor`]);
+	}
+	
+	Logout() {
+		console.log("Logging out the current user... " + this.getUserEmail());
+		this.afAuth.auth.signOut();
+		this.router.navigate(['login']);
+	}
 
 }
