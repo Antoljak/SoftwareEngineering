3d04b37982b26a8928faa26b18a94df3f4c1701e
unknown <fantoljak@ufl.edu>
2022-04-01 03:53:26 +0200

    Sprint 3 cypress tests

---
+++ go-angular/webapp/cypress/integration/SaveTest.js
@@ -0,0 +1,20 @@
+Cypress.on('uncaught:exception', (err, runnable) => {
+    return false
+})
+
+describe('Example', function() {
+    it('Showcase', function() {
+        cy.visit('localhost:4200/')
+        cy.contains('Sign in with email').click()
+        cy.get('#ui-sign-in-email-input').type("cypress@gmail.com")
+        cy.contains('Next').click()
+        cy.get('#ui-sign-in-password-input').type("password")
+        cy.get('.firebaseui-id-submit').click()
+        cy.wait(1000)
+        cy.get('.mat-icon').click()
+        cy.get('.mat-menu-content > :nth-child(2)').click()
+        cy.get('#mat-input-0').type("Test")
+        cy.get('#mat-input-1').type("Red")
+        cy.contains("Ok").click()
+    })
+})
